3b791420912e57b569ea18d963e9cfc3be640f0f
Mister-Clo <clotaire.tchoupou-ngakou@efrei.net>
2023-03-16 17:31:09 +0100

    Calculator with a Handler class

--- app/src/main/java/ls1/efrei/calculator/MainActivity.java
+++ app/src/main/java/ls1/efrei/calculator/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.webkit.ValueCallback;
 import android.webkit.WebView;
@@ -20,6 +21,7 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
     List<Integer> digits = new ArrayList<Integer>();
     List<Integer> operators = new ArrayList<Integer>();
+    private Handler handler;
     WebView webView;
     TextView operation;
     TextView result;
@@ -32,6 +34,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         webView = new WebView(this);
         webView.getSettings().setJavaScriptEnabled(true);
+        handler = new Handler();
         myButton = new Button(this);
         myButton.setId(2327);
         myButton.setLayoutParams(new LinearLayout.LayoutParams(0, LinearLayout.LayoutParams.MATCH_PARENT, 0.2f));
@@ -65,35 +68,52 @@ public class MainActivity extends AppCompatActivity {
         operators.add(R.id._multiply);
     }
 
+
+
     @SuppressLint("ResourceType")
     public void myClickHandler(View view) {
-        try{
-        String input = ((Button)view).getText().toString();
-        if(digits.contains(view.getId()) || operators.contains(view.getId())){
-            operation.setText(operation.getText().toString().concat(String.valueOf(input)));
-        }
-        if(view.getId() == 2327){
-                    webView.evaluateJavascript(operation.getText().toString(), new ValueCallback<String>() {
-                        @Override
-                        public void onReceiveValue(String s) {
-                           try {
-                               double output = Double.parseDouble(s);
-                               DecimalFormat df = new DecimalFormat("#.##########"); // set the format to two decimal places
-                               String formattedValue = df.format(output); // format the value as a string
-                               double roundedValue = Double.parseDouble(formattedValue);
-                               result.setText(String.valueOf(roundedValue));
-                               operation.setText("");
-                           }
-                           catch (NumberFormatException e) {
-                               Toast.makeText(getApplicationContext(), "Arithmetic Error", Toast.LENGTH_LONG).show();
-                               operation.setText("");
-                           }
-                        }
-                    });
-                }
-        }
-        catch (Exception e){
-            Toast.makeText(this, "Arithmetic Error", Toast.LENGTH_LONG).show();
-        }
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                    String input = ((Button)view).getText().toString();
+                    if(digits.contains(view.getId()) || operators.contains(view.getId())){
+                        handler.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                operation.setText(operation.getText().toString().concat(String.valueOf(input)));
+                            }
+                        });
+
+                    }
+                    if(view.getId() == 2327){
+                        webView.evaluateJavascript(operation.getText().toString(), new ValueCallback<String>() {
+                            @Override
+                            public void onReceiveValue(String s) {
+                                try {
+                                    double output = Double.parseDouble(s);
+                                    DecimalFormat df = new DecimalFormat("#.##########"); // set the format to two decimal places
+                                    String formattedValue = df.format(output); // format the value as a string
+                                    double roundedValue = Double.parseDouble(formattedValue);
+                                    handler.post(new Runnable() {
+                                        @Override
+                                        public void run() {
+                                            result.setText(String.valueOf(roundedValue));
+                                            operation.setText("");
+                                        }
+                                    });
+
+                                }
+                                catch (NumberFormatException e) {
+                                    Toast.makeText(getApplicationContext(), "Arithmetic Error", Toast.LENGTH_LONG).show();
+                                    operation.setText("");
+                                }
+                            }
+                        });
+                    }
+
+            }
+        };
+        runOnUiThread(runnable);
+
     }
 }
